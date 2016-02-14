40169d1189c25b9b0ee7efbbfedf9e5cefb00cc2
pipicom_ <info@filtermusic.net>
2016-02-14 21:48:54 +0100

    just a backup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # atm
-create an ATM example in HTML, CSS, JS
+create an ATM example using HTML, CSS, JS
+
+
+- Just for fun I have implemented a 3 allowed tries procedure. After the 3rd mistaken PIN input your transaction is cancelled.
+- Cancel and Back buttons are displayed where it makes sense. For example on screen no.4 (waiting for your money) it makes no sense for these buttons to be displayed. Also on the second screen (enter PIN) back button is redundant - the cancel button makes more sense here.
+- Code is W3C valid.
+- CSS browser prefixes were taken from http://pleeease.io/play. On a public project a CSS preprocessor would have taken care of this.
+-
--- scripts.js
+++ scripts.js
@@ -2,110 +2,111 @@
 (function($) {
     $(document).ready(function() {
 
-    /* ==========================================================================
-     GENERIC - insert credit card
-     ========================================================================== */
+        /* ==========================================================================
+         GENERIC
+         ========================================================================== */
 
-    $("input").keyup(function() {
-        $(this).val(this.value.match(/[0-9]*/));
-    });
+        /* allow only numbers on input fields  */
+        $("input").on( "keyup", function() {
+            $(this).val(this.value.match(/[0-9]*/));
+        });
 
-    /* preload the ad image */
-    $('<img src="images/ad1.jpg" />');
+        /* preload the ad image */
+        $('<img src="images/ad1.jpg" />');
 
-    /* go to another tab */
-    function goTo(destination, time){
-        setTimeout(function(){
-            $('a[href="#' + destination + '"]').tab('show');
-        },time)
-    }
+        /* generic function for redirection between tabs */
+        function goTo(destination, time){
+            setTimeout(function(){
+                $('a[href="#' + destination + '"]').tab('show');
+            },time)
+        }
 
-    $('form').attr('autocomplete', 'off');
+        var trial = 0; /* set tries variable  */
 
-    var trial = 0;
-    $(".canceltransaction").on('click', function() {
-        goTo('canceltransaction', 0);
-    });
+        $(".canceltransaction").on('click', function() {
+            goTo('canceltransaction', 0); /* when cancel button is clicked, redirect appropriately */
+        });
 
-    /* ==========================================================================
-     TAB 2 - enter PIN
-     ========================================================================== */
+        /* ==========================================================================
+         TAB 2 - enter PIN
+         ========================================================================== */
 
-    $("#pinfield").focus();
+        $('a[href="#pin"]').on('shown.bs.tab', function (e) {
+            $("#pinfield").focus(); /* focus on the field when shown  */
+        });
 
-    /* dont accept spaces */
-    $("#pinfield").on( "blur", function() {
-        var value = $( this ).val().trim();
-        $(this).val(value);
-    });
+        $('#pin form').submit(function(e) {
+        //$('#pin form :submit').click(function(e) {
+        //$('#pin').on('click','form :submit', function(e) {
 
-    $('#pin form').submit(function(e) {
-    //$('#pin form :submit').click(function(e) {
-    //$('#pin').on('click','form :submit', function(e) {
+            e.preventDefault(); /* dont submit form */
 
-        e.preventDefault();
+            if ( $('#pinfield').val() == '1234') { /* what happens when PIN is correct */
+                //goTo('selectamount', 0);
+                //$('a[href="#selectamount"]').tab('show');
+                $('ul.nav li:eq(2) a').tab('show');
+            } else { /* PIN is incorrect */
 
-        if ( $('#pinfield').val() == '1234') {
-            //goTo('selectamount', 0);
-            //$('a[href="#selectamount"]').tab('show');
-            $('ul.nav li:eq(2) a').tab('show');
-        } else {
+                trial ++; /* increment trial variable */
 
-            trial ++;
-
-            if (trial >= 3) {
-                goTo('canceltransaction', 0);
-            } else {
-                $('.alert').toggleClass('visible');
-                setTimeout(function(){
-                    $('.alert').toggleClass('visible');
-                },3000);
+                if (trial >= 3) {
+                    trial = 0; /* reset trial counter */
+                    goTo('canceltransaction', 0); /* cancel transaction */
+                } else {
+                    /* alert error */
+                    $('.alert').toggleClass('visible').html("Wrong PIN. Please try again. <br>You have " + (3 - trial) + " more tries.");
+                    setTimeout(function(){ /* hide error */
+                        $('.alert').toggleClass('visible');
+                    },3000);
+                }
             }
-        }
-    });
+        });
 
+        /* ==========================================================================
+         TAB 3 - select amount
+         ========================================================================== */
 
+        /* "other amount" button is clicked - show the form */
+        $('.customamount').on('click', function(){
+            $('#selectamount form').show();
+        });
 
-    /* ==========================================================================
-     TAB 3 - select amount
-     ========================================================================== */
+        // "other amount" form is submitted. There could be other validators here eg. allow only 20, 40, 60, 80
+        // currently it will accept any kind of number but only 5 digits.
+        $('#selectamount form').submit(function(e) {
+            e.preventDefault();
+            if ($("#selectamount input").val()) {
+                goTo('preparing', 0);
+            }
+        });
 
-    $('.customamount').on('click', function(){
-        $('#selectamount form').show();
-    });
+        /* ==========================================================================
+         TAB 4 - please wait
+         ========================================================================== */
 
-    $('#selectamount form').submit(function(e) {
-        e.preventDefault();
-        if ($("#selectamount input").val()) {
-            console.log($("input").val());
-            goTo('preparing', 0);
-        }
-    });
+        /* transaction is being processed. Please wait :) */
+        //$('a[href="#preparing"]').on('shown.bs.tab', function (e) {
+        //    goTo('showmethemoney', 5000);
+        //});
 
-    /* ==========================================================================
-     TAB 4
-     ========================================================================== */
+        /* ==========================================================================
+         TAB 5 - get your money and your card
+         ========================================================================== */
 
-    $('a[href="#preparing"]').on('shown.bs.tab', function (e) {
-        goTo('showmethemoney', 5000);
-    });
+        /* thank you for using the ATM - take your money and card and redirect to homepage */
+        $('a[href="#showmethemoney"]').on('shown.bs.tab', function (e) {
+            goTo('home', 5000);
+        });
 
-    /* ==========================================================================
-     TAB 5
-     ========================================================================== */
+        /* ==========================================================================
+         TAB 6 - Cancel transaction
+         ========================================================================== */
 
-    $('a[href="#showmethemoney"]').on('shown.bs.tab', function (e) {
-        goTo('home', 5000);
-    });
-
-    /* ==========================================================================
-     TAB 6 - Cancel transaction
-     ========================================================================== */
-
-    $('a[href="#canceltransaction"]').on('shown.bs.tab', function (e) {
-        trial = 0;
-        goTo('home', 5000);
-    });
+        /* screen to be visible only when transaction is cancelled because user wanted to or because PIN was mistaken for 3 times  */
+        $('a[href="#canceltransaction"]').on('shown.bs.tab', function (e) {
+            trial = 0;
+            goTo('home', 5000);
+        });
 
     });
 })(jQuery);
