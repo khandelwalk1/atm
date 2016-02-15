cc37b5be6244d3a42ec8d06de8c877aecf8b6603
pipicom_ <info@filtermusic.net>
2016-02-15 01:29:30 +0100

    final version

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ create an ATM example using HTML, CSS, JS
 
 
 - Just for fun I have implemented a 3 allowed tries procedure. After the 3rd mistaken PIN input your transaction is cancelled.
-- Cancel and Back buttons are displayed where it makes sense. For example on screen no.4 (waiting for your money) it makes no sense for these buttons to be displayed. Also on the second screen (enter PIN) back button is redundant - the cancel button makes more sense here.
+- Cancel and Back buttons are displayed where it makes sense. For example on screen no.5 (waiting for your money) it makes no sense for these buttons to be displayed.
+- Had to create one more screen (no4) where user enters custom amount of money.
+- App plays well even on IE9 (without animation of course).
+- Have used fontawesome library for the welcome screen icons.
 - Code is W3C valid.
 - CSS browser prefixes were taken from http://pleeease.io/play. On a public project a CSS preprocessor would have taken care of this.
-- dd
+- Images are shrinked using https://tinypng.com
--- scripts.js
+++ scripts.js
@@ -11,8 +11,9 @@
             $(this).val(this.value.match(/[0-9]*/));
         });
 
-        /* preload the ad image */
+        /* preload some images */
         $('<img src="images/ad1.jpg" />');
+        $('<img src="images/money.gif" />');
 
         /* generic function for redirection between tabs */
         function goTo(destination, time){
@@ -23,9 +24,28 @@
 
         var trial = 0; /* set tries variable  */
 
+        /* when cancel button is clicked, redirect appropriately */
         $(".canceltransaction").on('click', function() {
-            goTo('canceltransaction', 0); /* when cancel button is clicked, redirect appropriately */
+            goTo('canceltransaction', 0);
         });
+
+
+        // Javascript to enable link to tab
+        if(window.location.hash != "") {
+            $('.nav-tabs a[href="' + window.location.hash + '"]').tab('show');
+        }
+
+        // Change hash for page-reload
+        $('.nav-tabs a').on('shown.bs.tab', function (e) {
+            window.location.hash = e.target.hash.replace("#", "#");
+        });
+
+        /* back button or other internal link is clicked */
+        $(".backbutton, .internal_link").on('click', function() {
+            var backDestination = $(this).attr('href');
+            $('.nav-tabs a[href="' + backDestination + '"]').tab('show');
+        });
+
 
         /* ==========================================================================
          TAB 2 - enter PIN
@@ -36,15 +56,11 @@
         });
 
         $('#pin form').submit(function(e) {
-        //$('#pin form :submit').click(function(e) {
-        //$('#pin').on('click','form :submit', function(e) {
 
             e.preventDefault(); /* dont submit form */
 
-            if ( $('#pinfield').val() == '1234') { /* what happens when PIN is correct */
-                //goTo('selectamount', 0);
-                //$('a[href="#selectamount"]').tab('show');
-                $('ul.nav li:eq(2) a').tab('show');
+            if ( $('#pinfield').val() == '1234') { /* PIN is correct */
+                goTo('selectamount', 0);
             } else { /* PIN is incorrect */
 
                 trial ++; /* increment trial variable */
@@ -57,40 +73,35 @@
                     $('.alert').toggleClass('visible').html("Wrong PIN. Please try again. <br>You have " + (3 - trial) + " more tries.");
                     setTimeout(function(){ /* hide error */
                         $('.alert').toggleClass('visible');
-                    },3000);
+                    },2500);
                 }
             }
         });
 
         /* ==========================================================================
-         TAB 3 - select amount
+         TAB 4 - fill input field
          ========================================================================== */
-
-        /* "other amount" button is clicked - show the form */
-        $('.customamount').on('click', function(){
-            $('#selectamount form').show();
-        });
 
         // "other amount" form is submitted. There could be other validators here eg. allow only 20, 40, 60, 80
         // currently it will accept any kind of number but only 5 digits.
-        $('#selectamount form').submit(function(e) {
+        $('#customamount form').submit(function(e) {
             e.preventDefault();
-            if ($("#selectamount input").val()) {
+            if ($("#customamount input").val()) {
                 goTo('preparing', 0);
             }
         });
 
         /* ==========================================================================
-         TAB 4 - please wait
+         TAB 5 - please wait
          ========================================================================== */
 
         /* transaction is being processed. Please wait :) */
-        //$('a[href="#preparing"]').on('shown.bs.tab', function (e) {
-        //    goTo('showmethemoney', 5000);
-        //});
+        $('a[href="#preparing"]').on('shown.bs.tab', function (e) {
+            goTo('showmethemoney', 5000);
+        });
 
         /* ==========================================================================
-         TAB 5 - get your money and your card
+         TAB 6 - get your money and your card
          ========================================================================== */
 
         /* thank you for using the ATM - take your money and card and redirect to homepage */
@@ -99,11 +110,12 @@
         });
 
         /* ==========================================================================
-         TAB 6 - Cancel transaction
+         TAB 7 - Cancel transaction
          ========================================================================== */
 
         /* screen to be visible only when transaction is cancelled because user wanted to or because PIN was mistaken for 3 times  */
         $('a[href="#canceltransaction"]').on('shown.bs.tab', function (e) {
+            console.log('canc');
             trial = 0;
             goTo('home', 5000);
         });
